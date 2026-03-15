7be98ff64613a804e8282f238db3477941939485
Marva Akbar <marvaakbar321@gmail.com>
2026-03-15 11:30:08 +0500

    aapka message

---
+++ javascript/javascript.js
@@ -0,0 +1,105 @@
+// inspect wale page pe console ko click karne ke baad jab hum window likh ka enter karte hain to wahan javascript ki saari commands ya prompt aa jati hain.
+/*console.log('Hello')
+//  ctrl + slash karne se wo line comment ho jae gi.ye single line comment ke liye use hota haii.
+/* ye multi-line comment karne ke liye hai*/
+// alert ek popout hai jo display hota hai or braces() main hum wo hi likhte hain jo hamein chahiye hota hai.
+/*alert('Hello alert')*/
+// x = 10 -> value
+// variables ->
+// Register form:
+
+username = 'marva'
+email = 'marva@gmail.com' 
+city = 'faisalabad'
+code = 38000 
+
+console.log(40)
+console.log('Hello')
+console.log("Hello's")
+
+console.log(username)
+console.log(email)
+console.log(city)
+console.log(code)
+
+// ab in saare variables ko ek line main display karwane ke liye :
+console.log(username, email, city, code)
+
+// hum "" ye wali quoation tab use karte hain jab hum ne kisi word main ' ye lagaya ho jese hello's.
+// is tarah likhna hai number or characters ke liye
+// code
+// characters hain ye name, email or city
+// string '' , ""
+// string main numbers ko nhii rakhte 
+// string, numbers -> Data types
+// string ko hum ne quotes main rakhna hai
+// numbers ko hum ne simple likhna hai matlab koi quotation nhii lagani
+
+// String, Number -> Data Types
+// Variables
+
+// Important defines:
+//  keywords (var, let, const)
+// data types (string, number, boolean, null, undefined)
+// prompt
+
+// text => string (also called)
+// variable -> assign -> value
+// $user123$name = ''
+// _user123_name = ''
+console.log(username)
+// var
+var userName = 'Maryam'
+// const => constant is ka matlab hai ke hum is ko change nhii kar sakte lekin new const bna sakte 
+const username = 'Fatima' 
+console.log(userName)
+// declare
+let rollno = 1234;
+// prompt => user se input ya koi bhi information leni hoti hai us ko prompt kehte hain.
+let email;
+console.log(email)
+email = prompt("Enter your email:")
+document.getElementById("demo").innerHTML = `<h1> ${email}</h1>;`
+// is ko kehte hain DOM ke saath kaam karna..
+// document wali line add karne se hmara email console pe nhii show ho ga ,page par ho ga.
+// undefined ka matlab hai ke hum  
+// let main hum redeclare nhii kar sakte lekin value update kar sakte hain.
+// var main hum sab kuch kar sakte hain matlab hum redeclare kar sakte hain.
+// declare = > assign => value
+// let rollno = 1234;
+// userName => lowerCamelCase
+// underscore is (_).
+// data stores -> table, 
+// 123,fatima,505
+// 341,noor,707
+// CSV => File
+// clg and press enter se console.log khud likha jae ga.
+
+// Text + variable
+const userName = 'Fatima'
+// console.log('Hello', userName, 123, 'Welcome')
+console.log(`Hello ${username}`)
+
+// Operator
+// Arithmetic operator(*, +, -, %)
+// comparison operator()
+// logical operator()
+
+// operands
+
+let first_no = 2
+let second_no = 4
+let result ;
+result = first_no + second_no
+result = first_no - second_no
+result = first_no * second_no
+result = first_no % second_no
+result = first_no / second_no
+console.log(result);
+
+
+// console.log(first_no + second_no);
+// console.log(first_no - second_no);
+// console.log(first_no * second_no);
+// console.log(first_no % second_no);
+// console.log(first_no + second_no);
